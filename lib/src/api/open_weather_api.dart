c269eab28b0dcf941acf2e1aa0260c6842628698
Sobhie <sobhie.saad1@gmail.com>
2022-01-08 14:02:59 +0300

    Create splash screen

---
+++ lib/src/api/open_weather_api.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WetherAPI extends StatelessWidget {
+  // ignore: non_constant_identifier_names
+  final String api_key = "965ec8c752a72918604e7b2fa8d1d6ea";
+  const WetherAPI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/src/screens/splash.dart
+++ lib/src/screens/splash.dart
@@ -1,19 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:weather/src/home.dart';
 import 'package:weather/src/settings/settings_controller.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   final SettingsController settingsController;
+
   const SplashScreen({Key? key, required this.settingsController})
       : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late final Duration duration;
+  @override
+  void initState() {
+    animationController = AnimationController(duration: duration, vsync: this)
+      ..forward()
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                      HomePage(controller: widget.settingsController)));
+        }
+      });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [Container(
-          child: ,
-        )],
-      ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(children: <Widget>[
+        const Divider(
+          height: 240.0,
+          color: Colors.white,
+        ),
+        Image.asset(
+          'assets/logo.png',
+          fit: BoxFit.cover,
+          repeat: ImageRepeat.noRepeat,
+          width: 170.0,
+        ),
+        const Divider(
+          height: 105.2,
+          color: Colors.white,
+        ),
+      ]),
     );
   }
 }
