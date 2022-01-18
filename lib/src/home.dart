3419ecf152c6556234c145a6f3375041bd52181b
Sobhie <sobhie.saad1@gmail.com>
2022-01-19 01:04:13 +0300

    Add button navigation bar with 2 pages

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -20,57 +20,127 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+  PageController pageController = PageController();
+
   WeatherApiClient client = WeatherApiClient();
   List<Location> locations = [
     Location(
-        city: "Arbil", country: "Iraq", lat: "35.5636634", lon: "45.4695083")
+        city: "Sulaymaniyah",
+        country: "Iraq",
+        lat: "35.5636634",
+        lon: "45.4695083")
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-          title: const Text(
-            'Weather with tasks app',
-            style: TextStyle(color: Colors.black, fontSize: 18),
-          ),
-          centerTitle: true,
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.menu),
-            color: Colors.black,
-          ),
+        elevation: 0.0,
+        title: const Text(
+          'Weather with tasks app',
+          style: TextStyle(color: Colors.black, fontSize: 18),
         ),
-        body: FutureBuilder(
-          future: getCurrentWeather(locations[0]),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // CityDropDown(locations),
-                  createAppBar(locations, locations[0], context),
-                  weatherBox(snapshot.data as Weather),
-                  weatherDetailsBox(snapshot.data as Weather),
-                  forcastViewsHourly(locations[0]),
-                  const Divider(),
-                  forcastViewsDaily(locations[0]),
-                ],
-              );
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return Container();
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+          color: Colors.black,
+        ),
+      ),
+      body: SizedBox.expand(
+        child: PageView(
+          controller: pageController,
+          onPageChanged: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
           },
-        )
-        // bottomNavigationBar: BottomNavigationBar(
-        //   items: [],
-        // ),
-        );
+          children: [
+            weatherPage(),
+            Container(
+              child: const Text("2"),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 20,
+        mouseCursor: SystemMouseCursors.grab,
+        selectedIconTheme:
+            const IconThemeData(color: Colors.lightBlueAccent, size: 35),
+        selectedItemColor: Colors.lightBlueAccent,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        currentIndex: selectedIndex,
+        onTap: onTabTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.wb_sunny_rounded),
+            icon: Icon(Icons.wb_sunny_outlined),
+            label: 'Weather',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.task_alt_rounded),
+            icon: Icon(
+              Icons.task_alt_outlined,
+            ),
+            label: 'Tasks',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.chat),
+          //   label: 'Chats',
+          // ),
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+    pageController.animateToPage(index,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeOut);
+  }
+
+  Widget weatherPage() {
+    return FutureBuilder(
+      future: getCurrentWeather(locations[0]),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // CityDropDown(locations),
+              createAppBar(locations, locations[0], context),
+              weatherBox(snapshot.data as Weather),
+              weatherDetailsBox(snapshot.data as Weather),
+              forcastViewsHourly(locations[0]),
+              const Divider(),
+              forcastViewsDaily(locations[0]),
+            ],
+          );
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Container();
+      },
+    );
   }
 }
